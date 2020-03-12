0f535c30dfe6344d9adf8b6e4b46a5f69ab67939
charanjeetkau <ckbss97@gmail.com>
2020-03-12 16:12:21 +0530

    computing the results of all 3 operations

--- 3inputs.sh
+++ 3inputs.sh
@@ -2,7 +2,9 @@
 read -p "Enter first input number: " a
 read -p "Enter second input number: " b
 read -p "Enter third input number: " c
-res=$(($a+$b*$c))
+res=`expr "scale=4;$a+$b*$c" | bc -l`
 echo "$a + $b * $c = $res"
-res2=$(($a*$b+$c))
+res2=`expr "scale=4;$a*$b+$c" | bc -l`
 echo "$a * $b + $c = $res2"
+res3=`expr "scale=4;$c+$a/$b" | bc -l`
+echo "$c + $a / $b = $res3"
