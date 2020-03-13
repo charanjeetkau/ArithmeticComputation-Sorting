b046a30bff31af4ec0a9b1af7a2d7baa98b70688
charanjeetkau <ckbss97@gmail.com>
2020-03-13 13:36:08 +0530

    added: storing values from dictionary to array

--- 3inputs.sh
+++ 3inputs.sh
@@ -11,8 +11,17 @@ echo "$c + $a / $b = $res3"
 res4=`expr "scale=4;$a%$b+$c" | bc -l`
 echo "$a % $b + $c = $res4"
 
+declare -A results
 results[1]=$res1
 results[2]=$res2
 results[3]=$res3
 results[4]=$res4
 echo ${!results[@]} ' : ' ${results[@]}
+
+declare -a array
+array[0]=${results[1]}
+array[1]=${results[2]}
+array[2]=${results[3]}
+array[3]=${results[4]}
+echo 'Array: '${array[@]}
+
